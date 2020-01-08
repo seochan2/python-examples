7e835477db775bea52b5c25bc81d71bc9a6b65ef
seochan2 <seochan2@gmail.com>
2020-01-08 12:03:21 +0000

    init request session

--- wa-inspection/wa-inspection.py
+++ wa-inspection/wa-inspection.py
@@ -1,5 +1,14 @@
+import requests
+
 def main():
-    print('hi')
+    URL = ""
+
+    session = requests.Session()
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Connection": "Keep-Alive"
+    }
     
 if __name__ == "__main__":
     main()

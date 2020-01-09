b2a22ca1700c7bd3d3943b34037994468680c9da
seochan2 <seochan2@gmail.com>
2020-01-09 11:09:17 +0000

    get HTML a tag using BS4

--- wa-inspection/wa-inspection.py
+++ wa-inspection/wa-inspection.py
@@ -10,5 +10,12 @@ def main():
         "Connection": "Keep-Alive"
     }
     
+    bsObj = BeautifulSoup(session.get(URL, headers=headers).content, "html.parser")
+    
+    a_tag_list = bsObj.find_all('a')
+    
+    for a_tag in a_tag_list:
+        print(a_tag)
+    
 if __name__ == "__main__":
     main()
